65cfbf2b4f2f6340922456af1f9104b0d74bfc7d
webcqm <2650731512@qq.com>
2022-12-14 15:10:51 +0800

    update

---
+++ lib/main.dart
@@ -0,0 +1,275 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '个人中心',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routes:{
+        "new_page":(context) => NewRoute(),
+        "/":(context) => MyHomePage(title: 'Flutter Demo Home Page'), //注册首页路由
+      } ,
+     // home: const MyHomePage(title: '个人中心'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: MediaQuery.removePadding(
+        context: context,
+        //移除抽屉菜单顶部默认留白
+        removeTop: true,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 38.0),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: ClipOval(
+                      child: Image.asset(
+                        "imgs/avatar.png",
+                        width: 80,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "Wendux",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    leading: const Icon(Icons.add),
+                    title: const Text('Add account'),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.settings),
+                    title: const Text('Manage accounts'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  int _selectedIndex = 1;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  void _onAdd(){
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        leading: Builder(builder: (context) {
+          return IconButton(
+            icon: Icon(Icons.dashboard, color: Colors.white), //自定义图标
+            onPressed: () {
+              // 打开抽屉菜单
+              Scaffold.of(context).openDrawer();
+            },
+          );
+        }),
+        actions: <Widget>[ //导航栏右侧菜单
+          IconButton(icon: Icon(Icons.share), onPressed: () {}),
+        ],
+      ),
+      drawer: MyDrawer(), //抽屉
+      bottomNavigationBar: BottomNavigationBar( // 底部导航
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.business), label: 'Business'),
+          BottomNavigationBarItem(icon: Icon(Icons.school), label: 'School'),
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+                '姓名：陈启梦',
+              style: TextStyle(
+                color: Colors.blue
+              ),
+            ),
+            Text(
+                '学号：20191060165'
+            ),
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            TextButton(
+              child: Text("open new route"),
+              onPressed: () {
+                //导航到新路由
+                Navigator.pushNamed(context, "new_page");
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) {
+                //     return NewRoute();
+                //   }),
+                // );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _onAdd,//_incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+class TestFlowDelegate extends FlowDelegate {
+  EdgeInsets margin;
+
+  TestFlowDelegate({this.margin = EdgeInsets.zero});
+
+  double width = 0;
+  double height = 0;
+
+  @override
+  void paintChildren(FlowPaintingContext context) {
+    var x = margin.left;
+    var y = margin.top;
+    //计算每一个子widget的位置
+    for (int i = 0; i < context.childCount; i++) {
+      var w = context.getChildSize(i)!.width + x + margin.right;
+      if (w < context.size.width) {
+        context.paintChild(i, transform: Matrix4.translationValues(x, y, 0.0));
+        x = w + margin.left;
+      } else {
+        x = margin.left;
+        y += context.getChildSize(i)!.height + margin.top + margin.bottom;
+        //绘制子widget(有优化)
+        context.paintChild(i, transform: Matrix4.translationValues(x, y, 0.0));
+        x += context.getChildSize(i)!.width + margin.left + margin.right;
+      }
+    }
+  }
+
+  @override
+  Size getSize(BoxConstraints constraints) {
+    // 指定Flow的大小，简单起见我们让宽度竟可能大，但高度指定为200，
+    // 实际开发中我们需要根据子元素所占用的具体宽高来设置Flow大小
+    return Size(double.infinity, 200.0);
+  }
+
+  @override
+  bool shouldRepaint(FlowDelegate oldDelegate) {
+    return oldDelegate != this;
+  }
+}
+
+class NewRoute extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New route"),
+      ),
+      body: Center(
+        child: Column(
+        children: <Widget>[
+          Text("This is new route"),
+          Wrap(
+            spacing: 8.0, // 主轴(水平)方向间距
+            runSpacing: 4.0, // 纵轴（垂直）方向间距
+            alignment: WrapAlignment.center, //沿主轴方向居中
+            children: <Widget>[
+              Chip(
+                avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text('A')),
+                label: Text('Hamilton'),
+              ),
+              Chip(
+                avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text('M')),
+                label: Text('Lafayette'),
+              ),
+              Chip(
+                avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text('H')),
+                label: Text('Mulligan'),
+              ),
+              Chip(
+                avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text('J')),
+                label: Text('Laurens'),
+              ),
+            ],
+          ),
+          Flow(
+            delegate: TestFlowDelegate(margin: EdgeInsets.all(10.0)),
+            children: <Widget>[
+              Container(width: 80.0, height:80.0, color: Colors.red,),
+              Container(width: 80.0, height:80.0, color: Colors.green,),
+              Container(width: 80.0, height:80.0, color: Colors.blue,),
+              Container(width: 80.0, height:80.0,  color: Colors.yellow,),
+              Container(width: 80.0, height:80.0, color: Colors.brown,),
+              Container(width: 80.0, height:80.0,  color: Colors.purple,),
+            ],
+          )
+
+        ],
+        ),
+    ),
+    );
+  }
+}
+
+
